6338d2e746af5e5c132d36d38c528c28aa22ad3f
Aishat-Akinyemi <52966744+Aishat-Akinyemi@users.noreply.github.com>
2022-05-18 07:37:39 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  contractAddress: '0x35bC7b4B9D27Cb94428Be05B3fD03ab3eB6702Bf',
+  contractAddress: '0xf7BdED6CE7099Acca45503De5DeBC656ad969348',
   cUSDContractAddress: '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1',
   ipfs: 'https://ipfs.infura.io'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  contractAddress: '0x35bC7b4B9D27Cb94428Be05B3fD03ab3eB6702Bf',
+  contractAddress: '0xf7BdED6CE7099Acca45503De5DeBC656ad969348',
   cUSDContractAddress: '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1',
   ipfs: 'https://ipfs.infura.io'
 };
